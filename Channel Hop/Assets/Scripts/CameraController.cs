fbd9fa10a3e43cffb2aac4adc8818b3f371f7b61
JohnKA-04 <qtm0503@autuni.ac.nz>
2025-08-26 12:36:06 +1200

    Implemented Camera to follow player
    Also added a lookahead feature so that the player can see what is in front of them.

---
+++ Channel Hop/Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    [SerializeField] private Transform player;
+    [SerializeField] private float lookAheadDistance; // Distance the camera looks ahead of the player
+    [SerializeField] private float cameraSpeed; // Speed at which the camera moves
+    private float lookAhead; // Current look ahead distance
+    private void Update() // Runs continuously every frame
+    {
+        transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
+        lookAhead = Mathf.Lerp(lookAhead,(lookAheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
+        // time.deltatime makes it frame rate independent
+
+    }
+    
+
+}
