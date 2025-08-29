9cc853439f4685b37489b1f0ebff09aef32c6ef4
daenaram <157431231+daenaram@users.noreply.github.com>
2025-08-29 15:33:07 +1200

    New Assets implemented to pauseUI

--- Channel Hop/Assets/Scripts/PauseScript.cs
+++ Channel Hop/Assets/Scripts/PauseScript.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseScript : MonoBehaviour
 {
     [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private Button pauseButton;
 
     public void Awake()
     {
         pauseMenu.SetActive(false); // Ensure the pause menu is inactive at the start
+        pauseButton.interactable = true; // Ensure the pause button is interactable
     }
 
     public void Update()
@@ -17,22 +20,25 @@ public class PauseScript : MonoBehaviour
     {
         pauseMenu.SetActive(true); // Activate the pause menu
         Time.timeScale = 0; // Pause the game
+        pauseButton.interactable = false; // Disable the pause button
     }
 
     public void home()
     {
-
+        Debug.Log("going home");
     }
 
     public void resume()
     {
         pauseMenu.SetActive(false);
         Time.timeScale = 1; // Resume the game
+        pauseButton.interactable = true; // Disable the pause button
     }
 
     public void restart()
     {
-
+        // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Debug.Log("restarting");
     }
 
 }
