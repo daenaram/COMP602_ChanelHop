63b474060fa62f1692be713cd5a38b777e10f466
JCDC343 <211278250+jcGitqt@users.noreply.github.com>
2025-09-02 14:23:25 +1200

    Player health
    Implemented health hearts system and collectibles

---
+++ Channel Hop/Assets/Scripts/Health/Health.cs
@@ -0,0 +1,48 @@
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+    [SerializeField] private float startingHealth;
+    public float currentHealth { get; private set; }
+    private Animator anim;
+    private bool dead;
+
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    public void TakeDamage(float _damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+        
+        if(currentHealth > 0)
+        {
+            // player hurt
+            anim.SetTrigger("hurt");
+            // iframes
+        } 
+        else
+        {
+            if(!dead)
+            {
+                // player dead
+                anim.SetTrigger("die");
+                GetComponent<PlayerMovement>().enabled = false;
+                dead = true;
+            }
+         
+        }
+    }
+
+    // Add health (reference to HealthCollectible script)
+    public void AddHealth(float _value)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    }
+
+}
