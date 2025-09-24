b04bc3b34e791c37b94c15f526004cc3697c3901
JohnKA-04 <qtm0503@autuni.ac.nz>
2025-09-24 16:23:58 +1200

    Added camera, saw trap and health bar improvements
    Camera now zooms in or out depending on distance between players, improve health bar did no have p1 health and p2 health attached. Saw now inflicts damage to both players.

--- Channel Hop/Assets/Scripts/Traps/Enemy_Sideways.cs
+++ Channel Hop/Assets/Scripts/Traps/Enemy_Sideways.cs
@@ -47,7 +47,7 @@ public class Enemy_Sideways : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Player")
+        if(collision.tag == "Player1" || collision.tag == "Player2")
         {
             collision.GetComponent<Health>().TakeDamage(damage);
         }
