89ed7e7046d98a42c9df4f9f113fd63c18c6713a
daenaram <157431231+daenaram@users.noreply.github.com>
2025-10-16 21:51:36 +1300

    Code Update for GameOver and SaveUI

--- Channel Hop/Assets/Scripts/GameOverScript.cs
+++ Channel Hop/Assets/Scripts/GameOverScript.cs
@@ -10,23 +10,25 @@ public class GameOverScript : MonoBehaviour
     [SerializeField] private Health player2HP;
     [SerializeField] private PlayerRespawn player1Respawn;
     [SerializeField] private PlayerRespawn player2Respawn;
+    [SerializeField] SaveUI saveUI;
+    [SerializeField] private SaveController saveController;
     public bool isGameOver = false;
-    private PlayerMovement player1Movement;
-    private PlayerMovement player2Movement;
-    private PlayerAttackingScript player1Attacking;
-    private PlayerAttackingScript player2Attacking;
+    //private PlayerMovement player1Movement;
+    //private PlayerMovement player2Movement;
+    //private PlayerAttackingScript player1Attacking;
+    //private PlayerAttackingScript player2Attacking;
     private bool isRevived = false;
     private Animator player1Anim;
     private Animator player2Anim;
 
     void Start()
     {
-        player1Movement = GameObject.FindWithTag("Player1").GetComponent<PlayerMovement>();
-        player1Attacking = GameObject.FindWithTag("Player1").GetComponent<PlayerAttackingScript>();
+        //player1Movement = GameObject.FindWithTag("Player1").GetComponent<PlayerMovement>();
+        //player1Attacking = GameObject.FindWithTag("Player1").GetComponent<PlayerAttackingScript>();
         player1Anim = GameObject.FindWithTag("Player1").GetComponent<Animator>();
 
-        player2Movement = GameObject.FindWithTag("Player2").GetComponent<PlayerMovement>();
-        player2Attacking = GameObject.FindWithTag("Player2").GetComponent<PlayerAttackingScript>();
+        //player2Movement = GameObject.FindWithTag("Player2").GetComponent<PlayerMovement>();
+        //player2Attacking = GameObject.FindWithTag("Player2").GetComponent<PlayerAttackingScript>();
         player2Anim = GameObject.FindWithTag("Player2").GetComponent<Animator>();
 
         player1Respawn.SetGameOver(this);
@@ -69,10 +71,18 @@ public class GameOverScript : MonoBehaviour
 
     public void exit()
     {
-        // SceneManager.LoadScene("Menu");
         exitPopUp.SetActive(true);
     }
 
+    public void yesExit()
+    {
+        saveController.SaveGame("LastSave.json");
+        SceneManager.LoadScene("Menu");
+    }
+    public void noExit()
+    {
+         SceneManager.LoadScene("Menu");
+    }
     public void activateGameOver()
     {
         gameOver.SetActive(true);
