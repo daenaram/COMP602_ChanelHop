68c637392ecc7983db451f59c6d8ce445da56c90
JohnKA-04 <qtm0503@autuni.ac.nz>
2025-10-18 19:04:53 +1300

    Added a fixed camera to level 2 scene

---
+++ Channel Hop/Assets/Door.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] private Transform prevRoom;
+    [SerializeField] private Transform nextRoom;
+    [SerializeField] private CameraController1 cam;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player1") || collision.CompareTag("Player2"))
+        {
+            float targetX;
+
+            if (collision.transform.position.x < transform.position.x)
+            {
+                targetX = nextRoom.position.x;
+                cam.MoveToNewRoom(nextRoom);
+            }
+            else
+            {
+                targetX = prevRoom.position.x;
+                cam.MoveToNewRoom(prevRoom);
+            }
+
+            Debug.Log($"Camera current X: {cam.transform.position.x}, Target X: {targetX}");
+        }
+    }
+}
+
