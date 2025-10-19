7ad8ce98db969ed7fccc580fe175480e250cc72b
JohnKA-04 <qtm0503@autuni.ac.nz>
2025-10-19 21:50:47 +1300

    Updated level2 and added door scripts

--- Channel Hop/Assets/Scripts/Door/Door.cs
+++ Channel Hop/Assets/Scripts/Door/Door.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Door : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Transform prevRoom;
+    [SerializeField] private Transform nextRoom;
+    [SerializeField] private CameraController2 cam;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Player1") || collision.CompareTag("Player2"))
+        {
+            float targetX;
+
+            if (collision.transform.position.x < transform.position.x)
+            {
+                targetX = nextRoom.position.x;
+                cam.MoveToNewRoom(nextRoom);
+            }
+            else
+            {
+                targetX = prevRoom.position.x;
+                cam.MoveToNewRoom(prevRoom);
+            }
+
+            Debug.Log($"Camera current X: {cam.transform.position.x}, Target X: {targetX}");
+        }
     }
 }
+
